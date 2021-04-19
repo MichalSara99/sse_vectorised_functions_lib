16e7aef311e890bfed4d52aad1363c4414cbfb1e
Michal Sara <michal.sara99@gmail.com>
2021-04-19 16:49:52 +0200

    CHANGEs: 1) moved tancot_functions_.asm to asm_x86 folder 2) added asm_x86_incs folder for all the assembly includes with all the prototypes and constants 3) started working on the assembly code to allow for aligned memory block of any length not just multiple of 2 or 4 - started on basic_functions

--- asm_x86/basic_functions_.asm
+++ asm_x86/basic_functions_.asm
@@ -1,39 +1,36 @@
-								.model flat,c
-								.const
-								align 16
-;; =========================================================== GENERAL ====================================================
+include asm_x86_incs/basic_functions.inc
 
-int_abs_mask_ps					dword	7fffffffh,7fffffffh,7fffffffh,7fffffffh	
-int_abs_mask_pd					qword	7fffffffffffffffh,7fffffffffffffffh
-
-								.code
-
+.code
 ;;			extern "C" bool maxs_sse_ps(float const* x,float const* y, int n, float *out);
-maxs_sse_ps						proc
-								push ebp
-								mov ebp,esp
-								push ebx
-								push esi
+maxs_sse_ps						proc uses ebx esi,
+											x_ptr:ptr real4, 
+											y_ptr:ptr real4,
+											n_arg:dword,
+											out_ptr:ptr real4
 
 								xor eax,eax
-
-								mov ecx,[ebp + 16]
-								cmp ecx,4
-								jl Done
-
-								mov ebx,[ebp + 8]
+								;; first three checks are for alignment => this will be 
+								;; removed once it is guaranteed through C++ object
+								mov ebx,x_ptr
 								test ebx,0fh
-								jnz Done
+								jnz done
 
-								mov esi,[ebp + 12]
+								mov esi,y_ptr
 								test esi,0fh
-								jnz Done
+								jnz done
 
-								mov edx,[ebp + 20]
+								mov edx,out_ptr
 								test edx,0fh
-								jnz Done
+								jnz done
 
-								and ecx,0fffffffeh
+								mov ecx,n_arg
+								cmp ecx,4
+								jl too_short
+
+								mov eax,ecx
+								and ecx,0fffffffch
+								sub eax,ecx
+								shr ecx,2
 
 					@@:			movaps xmm0,xmmword ptr [ebx]
 								movaps xmm1,xmmword ptr [esi]
@@ -42,44 +39,71 @@ maxs_sse_ps						proc
 								add ebx,16
 								add esi,16
 								add edx,16
-								sub ecx,4
+								dec ecx
 								jnz @B
 
+								mov ecx,eax
+				too_short:		or ecx,ecx
 								mov eax,1
+								jz done
 
-				Done:			pop esi
-								pop ebx
-								mov esp,ebp
-								pop ebp
-								ret
+								movaps xmm0,xmmword ptr [ebx]
+								movaps xmm1,xmmword ptr [esi]
+								maxps xmm0,xmm1
+
+								cmp ecx,1
+								je short one_left
+								cmp ecx,2
+								je short two_left
+								cmp ecx,3
+								je short three_left
+								jmp done
+
+				one_left:		movss real4 ptr [edx],xmm0
+								jmp done
+				two_left:		insertps xmm2,xmm0,01000000b
+								movss real4 ptr [edx],xmm0
+								movss real4 ptr [edx + 4],xmm2	
+								jmp done
+				three_left:		insertps xmm2,xmm0,01000000b
+								insertps xmm3,xmm0,10000000b
+								movss real4 ptr [edx],xmm0
+								movss real4 ptr [edx + 4],xmm2
+								movss real4 ptr [edx + 8],xmm3
+
+				done:			ret
 maxs_sse_ps						endp
 
 ;;			extern "C" bool maxs_sse_pd(float const* x,float const* y, int n, float *out);
-maxs_sse_pd						proc
-								push ebp
-								mov ebp,esp
-								push ebx
-								push esi
+maxs_sse_pd						proc uses ebx esi,
+											x_ptr:ptr real8, 
+											y_ptr:ptr real8,
+											n_arg:dword,
+											out_ptr:ptr real8
 
 								xor eax,eax
-
-								mov ecx,[ebp + 16]
-								cmp ecx,2
-								jl Done
-
-								mov ebx,[ebp + 8]
+								;; first three checks are for alignment => this will be 
+								;; removed once it is guaranteed through C++ object
+								mov ebx,x_ptr
 								test ebx,0fh
-								jnz Done
+								jnz done
 
-								mov esi,[ebp + 12]
+								mov esi,y_ptr
 								test esi,0fh
-								jnz Done
+								jnz done
 
-								mov edx,[ebp + 20]
+								mov edx,out_ptr
 								test edx,0fh
-								jnz Done
+								jnz done
 
+								mov ecx,n_arg
+								cmp ecx,2
+								jl too_short
+
+								mov eax,ecx
 								and ecx,0fffffffeh
+								shr ecx,1
+								and eax,1h
 
 					@@:			movapd xmm0,xmmword ptr [ebx]
 								movapd xmm1,xmmword ptr [esi]
@@ -88,44 +112,50 @@ maxs_sse_pd						proc
 								add ebx,16
 								add esi,16
 								add edx,16
-								sub ecx,2
+								dec ecx
 								jnz @B
 
+								mov ecx,eax
+			too_short:			or ecx,ecx
 								mov eax,1
+								jz done
 
-				Done:			pop esi
-								pop ebx
-								mov esp,ebp
-								pop ebp
-								ret
+								movapd xmm0,xmmword ptr [ebx]
+								movapd xmm1,xmmword ptr [esi]
+								maxpd xmm0,xmm1
+								movsd real8 ptr [edx],xmm0
+
+				done:			ret
 maxs_sse_pd						endp
 
 ;;			extern "C" bool mins_sse_ps(float const* x,float const* y, int n, float *out);
-mins_sse_ps						proc
-								push ebp
-								mov ebp,esp
-								push ebx
-								push esi
-
+mins_sse_ps						proc uses ebx esi,
+											x_ptr:ptr real4, 
+											y_ptr:ptr real4,
+											n_arg:dword,
+											out_ptr:ptr real4
 								xor eax,eax
 
-								mov ecx,[ebp + 16]
-								cmp ecx,4
-								jl Done
-
-								mov ebx,[ebp + 8]
+								mov ebx,x_ptr
 								test ebx,0fh
-								jnz Done
+								jnz done
 
-								mov esi,[ebp + 12]
+								mov esi,y_ptr
 								test esi,0fh
-								jnz Done
+								jnz done
 
-								mov edx,[ebp + 20]
+								mov edx,out_ptr
 								test edx,0fh
-								jnz Done
+								jnz done
 
-								and ecx,0fffffffeh
+								mov ecx,n_arg
+								cmp ecx,4
+								jl too_short
+
+								mov eax,ecx
+								and ecx,0fffffffch
+								sub eax,ecx
+								shr ecx,2
 
 					@@:			movaps xmm0,xmmword ptr [ebx]
 								movaps xmm1,xmmword ptr [esi]
@@ -134,44 +164,71 @@ mins_sse_ps						proc
 								add ebx,16
 								add esi,16
 								add edx,16
-								sub ecx,4
+								dec ecx
 								jnz @B
 
+								mov ecx,eax
+				too_short:		or ecx,ecx
 								mov eax,1
+								jz done
 
-				Done:			pop esi
-								pop ebx
-								mov esp,ebp
-								pop ebp
-								ret
+								movaps xmm0,xmmword ptr [ebx]
+								movaps xmm1,xmmword ptr [esi]
+								minps xmm0,xmm1
+
+								cmp ecx,1
+								je short one_left
+								cmp ecx,2
+								je short two_left
+								cmp ecx,3
+								je short three_left
+
+				one_left:		movss real4 ptr [edx],xmm0
+								jmp short done
+
+				two_left:		insertps xmm1,xmm0,01000000b
+								movss real4 ptr [edx],xmm0
+								movss real4 ptr [edx + 4],xmm1
+								jmp short done
+
+				three_left:		insertps xmm1,xmm0,01000000b
+								insertps xmm2,xmm0,10000000b
+								movss real4 ptr [edx],xmm0
+								movss real4 ptr [edx + 4],xmm1
+								movss real4 ptr [edx + 8],xmm2
+
+				done:			ret
 mins_sse_ps						endp
 
 ;;			extern "C" bool mins_sse_pd(float const* x,float const* y, int n, float *out);
-mins_sse_pd						proc
-								push ebp
-								mov ebp,esp
-								push ebx
-								push esi
+mins_sse_pd						proc uses ebx esi,
+											x_ptr:ptr real8, 
+											y_ptr:ptr real8,
+											n_arg:dword,
+											out_ptr:ptr real8
 
 								xor eax,eax
 
-								mov ecx,[ebp + 16]
-								cmp ecx,2
-								jl Done
-
-								mov ebx,[ebp + 8]
+								mov ebx,x_ptr
 								test ebx,0fh
-								jnz Done
+								jnz done
 
-								mov esi,[ebp + 12]
+								mov esi,y_ptr
 								test esi,0fh
-								jnz Done
+								jnz done
 
-								mov edx,[ebp + 20]
+								mov edx,out_ptr
 								test edx,0fh
-								jnz Done
+								jnz done
 
+								mov ecx,n_arg
+								cmp ecx,2
+								jl too_short
+
+								mov eax,ecx
 								and ecx,0fffffffeh
+								shr ecx,1
+								and eax,1h
 
 					@@:			movapd xmm0,xmmword ptr [ebx]
 								movapd xmm1,xmmword ptr [esi]
@@ -180,16 +237,20 @@ mins_sse_pd						proc
 								add ebx,16
 								add esi,16
 								add edx,16
-								sub ecx,2
+								dec ecx
 								jnz @B
 
+								mov ecx,eax
+			too_short:			or ecx,ecx
 								mov eax,1
+								jz short done
 
-				Done:			pop esi
-								pop ebx
-								mov esp,ebp
-								pop ebp
-								ret
+								movapd xmm0,xmmword ptr [ebx]
+								movapd xmm1,xmmword ptr [esi]
+								minpd xmm0,xmm1
+								movsd real8 ptr [edx],xmm0
+
+				done:			ret
 mins_sse_pd						endp
 
 ;;			extern "C" bool min_sse_ps(float const* x, int n, float* out);
@@ -387,7 +448,7 @@ abs_sse_ps						proc
 								and ecx,0fffffffeh
 
 					@@:			movaps xmm0,xmmword ptr [ebx]
-								andps xmm0,xmmword ptr [int_abs_mask_ps]
+								andps xmm0,xmmword ptr [abs_mask_ps]
 								movaps xmmword ptr [edx],xmm0
 								add ebx,16
 								add edx,16
@@ -425,7 +486,7 @@ abs_sse_pd						proc
 								and ecx,0fffffffeh
 
 					@@:			movapd xmm0,xmmword ptr [ebx]
-								andpd xmm0,xmmword ptr [int_abs_mask_ps]
+								andpd xmm0,xmmword ptr [abs_mask_pd]
 								movapd xmmword ptr [edx],xmm0
 								add ebx,16
 								add edx,16
